803cd01eb2c644b9a3d729f616460cb8ff92f05d
chakbun <cheungjaben@hotmail.com>
2019-09-20 23:05:24 +0800

    do something wrong

--- letsdraw/CanvasView.swift
+++ letsdraw/CanvasView.swift
@@ -10,12 +10,28 @@ import UIKit
 
 class CanvasView: UIView {
     
+    var points: [CGPoint] = []
+    
     override func draw(_ rect: CGRect) {
-        
+        if points.count > 0 {
+            let context = UIGraphicsGetCurrentContext()
+            for (index, point) in points.enumerated() {
+                if index == 0 {
+                    context?.move(to: point)
+                }else {
+                    context?.addLine(to: point)
+                }
+            }
+            UIColor.red.set()
+            context?.setLineWidth(1.0)
+            context?.strokePath()
+        }
     }
 
     @objc func panActionWith(gesture: UIPanGestureRecognizer) -> Void {
-        print("hello view")
+//        let point = gesture.translation(in: self)
+//        self.points.append(point)
+//        self.setNeedsDisplay()
     }
     
 }
