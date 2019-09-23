1a1ce61faf7b30f26de5354a71a7d0e6ad22266b
chakbun <cheungjaben@hotmail.com>
2019-09-23 22:20:23 +0800

    change drawing method

--- letsdraw/CanvasView.swift
+++ letsdraw/CanvasView.swift
@@ -10,35 +10,42 @@ import UIKit
 
 class CanvasView: UIView {
     
-    var points: [CGPoint] = []
-    
-    override func draw(_ rect: CGRect) {
-        if points.count > 0 {
-            let context = UIGraphicsGetCurrentContext()
-            for (index, point) in points.enumerated() {
-                if index == 0 {
-                    context?.move(to: point)
-                }else {
-                    context?.addLine(to: point)
-                }
-            }
-            UIColor.red.set()
-            context?.setLineWidth(1.0)
-            context?.strokePath()
-        }
-    }
+    var drawPath: UIBezierPath?
+    var drawLayer: CAShapeLayer?
 }
 
 extension CanvasView {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-
+        if event?.allTouches?.count == 1 {
+            let point = (touches.first?.location(in: self))!
+            self.drawPath = UIBezierPath.init()
+            self.drawPath?.lineWidth = 2.0
+            self.drawPath?.move(to: point)
+            
+            if let path = self.drawPath {
+                let shape = CAShapeLayer.init()
+                shape.path = path.cgPath
+                shape.backgroundColor = UIColor.clear.cgColor
+                shape.fillColor = UIColor.clear.cgColor
+                shape.lineCap = .round
+                shape.lineJoin = .round
+                shape.strokeColor = UIColor.red.cgColor
+                shape.lineWidth = 2.0
+                self.layer.addSublayer(shape)
+                drawLayer = shape;
+            }
+        }
     }
+    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let point = (touches.first?.location(in: self))!
-        self.points.append(point)
-        self.setNeedsDisplay()
+        if event?.allTouches?.count == 1 {
+            let point = (touches.first?.location(in: self))!
+            drawPath?.addLine(to: point)
+            drawLayer?.path = drawPath?.cgPath
+        }
     }
+    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.points.removeAll()
+        
     }
 }
